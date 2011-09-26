2727a43b9d9c8b79464baa4c2f5fa071e7823126
Praveen Mookoni <mookonip@yahoo.com>
2011-09-25 20:35:10 -0400

    Initial Commit Madhs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Madhs::Application.config.secret_token = '1fc229d1af4d772e69b8ac98c9bfd401371d23a558e41badd7e420e5d952cab1c59fa19427d8c9ac7c68655484ab526dcecac813fc0ac4d2278cfc452df0fb1b'
